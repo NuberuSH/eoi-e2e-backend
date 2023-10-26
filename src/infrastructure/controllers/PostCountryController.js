df01971bc17b243a55fd44fa1a71c62b56023dac
Alexis Plettener <alexisplettener@gmail.com>
2023-10-26 17:29:26 +0100

    Fix PostCountryController

--- src/infrastructure/controllers/PostCountryController.js
+++ src/infrastructure/controllers/PostCountryController.js
@@ -11,25 +11,30 @@ export class PostCountryController {
   }
 
   execute = async (ctx) => {
-    const { name, temperature } = ctx.request.body;
+    const { name, temperature } = ctx.body;
 
     if (!name || !temperature) {
       console.log("Invalid request body");
       ctx.status = 400;
-      ctx.body = { error: 'Invalid request body' };
+      ctx.body = { error: "Invalid request body" };
       return;
     }
 
     try {
-      console.log("Executing registerCountry with name:", name, "and temperature:", temperature);
+      console.log(
+        "Executing registerCountry with name:",
+        name,
+        "and temperature:",
+        temperature,
+      );
       await this.registerCountry.execute(name, temperature);
       console.log("Registration successful.");
       ctx.status = 201;
-      ctx.body = { status: 'ok' };
+      ctx.body = { status: "ok" };
     } catch (error) {
       console.error("Error while executing registerCountry:", error);
       ctx.status = 500;
-      ctx.body = { error: 'Internal Server Error' };
+      ctx.body = { error: "Internal Server Error" };
     }
   };
 }
--- src/infrastructure/controllers/PostCountryController.test.js
+++ src/infrastructure/controllers/PostCountryController.test.js
@@ -1,86 +1,96 @@
-import { describe, it, expect, vi } from "vitest"
-import { PostCountryController } from "./PostCountryController.js"
-import { ZodError } from "zod"
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { PostCountryController } from "./PostCountryController.js";
+import { ZodError } from "zod";
 
 describe("PostCountryController", () => {
-  it("invokes the use case", async () => {
-    const registerCountry = { execute: vi.fn() }
-    const postCountryController = new PostCountryController(registerCountry)
-    const name = "Spain"
-    const temperature = 30
+  let res;
+  let json;
+  let registerCountry;
+  let postCountryController;
+
+  beforeEach(() => {
+    json = vi.fn();
+    res = {
+      status: () => ({ json }),
+    };
+    registerCountry = { execute: vi.fn() };
+    postCountryController = new PostCountryController(registerCountry);
+  });
+
+  it("calls registerCountry with the correct parameters", async () => {
+    const name = "Spain";
+    const temperature = 30;
     const req = {
       body: {
         name,
-        temperature
+        temperature,
       },
-    }
-    const json = vi.fn()
-    const res = {
-      status: () => ({ json }),
-    }
+    };
 
-    await postCountryController.execute(req, res)
+    await postCountryController.execute(req, res);
 
-    expect(registerCountry.execute).toHaveBeenCalledWith(name,
-      temperature)
-  })
+    await expect(registerCountry.execute).toHaveBeenCalledWith(
+      name,
+      temperature,
+    );
+  });
 
   it("responds with status 200", async () => {
-    const registerCountry = { execute: vi.fn() }
-    const postCountryController = new PostCountryController(registerCountry)
-    const name = "Spain"
-    const temperature = 30
+    const registerCountry = { execute: vi.fn() };
+    const postCountryController = new PostCountryController(registerCountry);
+    const name = "Spain";
+    const temperature = 30;
     const req = {
       body: {
         name,
-        temperature
+        temperature,
       },
-    }
-    const json = vi.fn()
+    };
+    const json = vi.fn();
     const res = {
       status: () => ({ json }),
-    }
+    };
 
-    await postCountryController.execute(req, res)
+    await postCountryController.execute(req, res);
 
-    await expect(json).toHaveBeenCalledWith({ status: "ok" })
-  })
+    await expect(json).toHaveBeenCalledWith({ status: "ok" });
+  });
 
   it("throws a zod error if temperature is not defined", async () => {
-    const registerCountry = { execute: vi.fn() }
-    const postCountryController = new PostCountryController(registerCountry)
-    const name = "Spain"
+    const registerCountry = { execute: vi.fn() };
+    const postCountryController = new PostCountryController(registerCountry);
+    const name = "Spain";
     const req = {
       body: {
         name,
       },
-    }
-    const json = vi.fn()
+    };
+    const json = vi.fn();
     const res = {
       status: () => ({ json }),
-    }
+    };
 
-    const result = postCountryController.execute(req, res)
+    const result = postCountryController.execute(req, res);
 
-    await expect(result).rejects.toBeInstanceOf(ZodError)
-  })
+    await expect(result).rejects.toBeInstanceOf(ZodError);
+  });
 
   it("throws a zod error if name is not defined", async () => {
-    const registerCountry = { execute: vi.fn() }
-    const postCountryController = new PostCountryController(registerCountry)
-    const temperature = 30
+    const registerCountry = { execute: vi.fn() };
+    const postCountryController = new PostCountryController(registerCountry);
+    const temperature = 30;
     const req = {
       body: {
         temperature,
       },
-    }
-    const json = vi.fn()
+    };
+    const json = vi.fn();
     const res = {
       status: () => ({ json }),
-    }
+    };
 
-    const result = postCountryController.execute(req, res)
+    const result = postCountryController.execute(req, res);
 
-    await expect(result).rejects.toBeInstanceOf(ZodError)
-  })
-})
+    await expect(result).rejects.toBeInstanceOf(ZodError);
+  });
+});
